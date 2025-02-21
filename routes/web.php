2d74a587c0578323c63acf43c4a219cd9005b1a8
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-02-21 20:34:15 +0100

    update routage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ Route::get('/', [QuestionController::class, 'index'])->name('home');
 Route::get('/search', [QuestionController::class, 'search'])->name('questions.search');
 Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
 Route::get('/popular', [QuestionController::class, 'popular'])->name('questions.popular');
-    Route::get('/recent', [QuestionController::class, 'recent'])->name('questions.recent');
+Route::get('/recent', [QuestionController::class, 'recent'])->name('questions.recent');
+Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
 Route::middleware(['auth', 'verified'])->group(function () {
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
@@ -22,11 +23,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/dashboard/answer/{question}', [DashboardController::class, 'storeAnswer'])->name('dashboard.storeAnswer');
 
     // Questions
-    Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
-    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create')->middleware('auth');
-    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store')->middleware('auth');
-   
-    //Route::get('/search', [QuestionController::class, 'search'])->name('questions.search');
+    
+    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
+    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
     
     // Réponses
     Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
@@ -36,21 +35,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
     Route::post('/questions/{question}/favorite', [FavoriteController::class, 'toggle'])->name('questions.favorite');;
     // Profil
-   // Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-   // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-   // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-   // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-    
-    // Autres routes pour le profil
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::get('/profile/favorites', [ProfileController::class, 'favorites'])->name('profile.favorites');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/* Routes publiques
-Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
-Route::get('/questions/{question}', action: [QuestionController::class, 'show'])->name('questions.show');
-Route::get('/questions/search', [QuestionController::class, 'search'])->name('questions.search');*/
